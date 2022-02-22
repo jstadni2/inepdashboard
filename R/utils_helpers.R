b75621b72b1d3905ed28c7dac7a12c83372e7902
jstadni2 <jstadni2@illinois.edu>
2022-02-22 12:31:52 -0600

    Implement and test helper function: percent

--- R/utils_helpers.R
+++ R/utils_helpers.R
@@ -9,6 +9,25 @@
 # utils function for calculating percent
 # used in census.R and profile server script
 # eg:
+
+#' Create percentage variable in data.frame
+#' 
+#' \code{percent} returns a data.frame with a percentage variable for the given numerator and denominator.
+#'
+#' @param df A data.frame containing numerator and denominator variable used for the percent variable.
+#' @param percent_name A name for the resulting percent variable.
+#' @param num The name of a numeric variable in \code{df} used as the numerator for calculating the percent variable.
+#' @param denom The name of a numeric variable in \code{df} used as the denominator for calculating the percent variable.
+#'
+#' @return A data.frame containing the percent variable.
+#' @export # export not necessary? 
+#'
+#' @examples
+percent <- function(df, percent_name, num, denom) {
+  df[[percent_name]] <- round(100 * (df[[num]] / df[[denom]]))
+  df
+}
+
 # snap_ed_eligibility_tracts$snap_eligibility_percent <-
 #   round(
 #     100 * (
--- tests/testthat/test-utils_helpers.R
+++ tests/testthat/test-utils_helpers.R
@@ -1,3 +1,11 @@
-test_that("multiplication works", {
-  expect_equal(2 * 2, 4)
+test_that("percent adds percent variable to data.frame", {
+  a <- seq(0, 10, length.out = 10)
+  b <- seq(3, 18, length.out = 10)
+  
+  df_in <- data.frame(a, b)
+  
+  df_out <- df_in
+  df_out$percent <- round(100 * (df_in$a / df_in$b))
+  
+  expect_equal(df_out, percent(df_in, "percent", "a", "b"))
 })
