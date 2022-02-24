b2eea30c7db1ab912dd15bb51485fc79f5f90591
jstadni2 <jstadni2@illinois.edu>
2022-02-24 14:17:50 -0600

    Add PEARS module exports to /data-raw

--- data-raw/pears.R
+++ data-raw/pears.R
@@ -1,17 +1,17 @@
 ## code to prepare `pears` dataset goes here
 
-sites <- readxl::read_xlsx("Sites_Export.xlsx", sheet = "Site Data") #update file
+sites <- readxl::read_xlsx("data-raw/Sites_Export.xlsx", sheet = "Site Data") #update file
 
-pa <- readxl::read_xlsx("Program_Activities_Export.xlsx", sheet = "Program Activity Data") #update file
+pa <- readxl::read_xlsx("data-raw/Program_Activities_Export.xlsx", sheet = "Program Activity Data") #update file
 
-ia <- readxl::read_xlsx("Indirect_Activities_Export.xlsx", sheet = "Indirect Activity Data") #update file
-ia_ic <- readxl::read_xlsx("Indirect_Activities_Export.xlsx", sheet = "Intervention Channels")
+ia <- readxl::read_xlsx("data-raw/Indirect_Activities_Export.xlsx", sheet = "Indirect Activity Data") #update file
+ia_ic <- readxl::read_xlsx("data-raw/Indirect_Activities_Export.xlsx", sheet = "Intervention Channels")
 
-pse <- readxl::read_xlsx("PSE_Site_Activities_Export.xlsx", sheet = "PSE Data") #update file
+pse <- readxl::read_xlsx("data-raw/PSE_Site_Activities_Export.xlsx", sheet = "PSE Data") #update file
 
-coa <- readxl::read_xlsx("Coalitions_Export.xlsx", sheet = "Coalition Data") #update file
-coa_members <- readxl::read_xlsx("Coalitions_Export.xlsx", sheet = "Members")
+coa <- readxl::read_xlsx("data-raw/Coalitions_Export.xlsx", sheet = "Coalition Data") #update file
+coa_members <- readxl::read_xlsx("data-raw/Coalitions_Export.xlsx", sheet = "Members")
 
-part <- readxl::read_xlsx("Partnerships_Export.xlsx", sheet = "Partnership Data") #update file
+part <- readxl::read_xlsx("data-raw/Partnerships_Export.xlsx", sheet = "Partnership Data") #update file
 
 # usethis::use_data(pears, overwrite = TRUE)
