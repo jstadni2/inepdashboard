3a7abbbb6f135655daa5ffbadffe2d6b9d3ff206
jstadni2 <jstadni2@illinois.edu>
2022-05-16 16:00:00 -0500

    Download Health_Center_Service_Delivery_and_LookAlike_Sites.csv

--- data-raw/community_sites.R
+++ data-raw/community_sites.R
@@ -19,7 +19,7 @@ system("docker run --name selchrome -d -p 4445:4444 selenium/standalone-chrome" 
 # fprof <- RSelenium::makeFirefoxProfile(list(browser.download.dir = "D:/temp"))
 # fprof.setAcceptUntrustedCertificates(TRUE)
 
-file_path <- getwd() %>% stringr::str_replace_all("/", "\\\\\\")
+# file_path <- getwd() %>% stringr::str_replace_all("/", "\\\\\\")
 # 
 # eCaps <- list(chromeOptions =
 #                 list(
@@ -166,17 +166,51 @@ remDr$findElement(using = "css selector", value = "#downloadCSV > button")$click
 
 # Move file from Downloads to data-raw
 
+# Get input path and container name programmatically
 system("cmd.exe", input = 'docker cp selchrome:/home/seluser/Downloads/data.csv "C:\\Users\\jstadni2\\Box\\FCS Data Analyst\\External Community Profile\\inepdashboard\\data-raw"')
+system("cmd.exe", input = 'docker exec selchrome rm -rf /home/seluser/Downloads/data.csv"')
 
 dest_path <- "C:\\Users\\jstadni2\\Box\\FCS Data Analyst\\External Community Profile\\inepdashboard\\data-raw\\"
 
 file.rename(paste0(dest_path, "data.csv"),
             paste0(dest_path, "head_start_centers.csv"))
 
-# Rename file?
+# import head_start_centers.csv and prep it for community_sites
+
+# Alternative: figure out how to get chrome profile working
 
 # Federally Qualified Health Centers
 # https://data.hrsa.gov/geo
+
+# remDr$navigate("https://data.hrsa.gov/geo")
+# 
+# # Select State/Territory > Illinois
+# remDr$findElement(using = "css selector", value = "#stateSelection > option:nth-child(18)")$clickElement()
+# # Click Submit
+# remDr$findElement(using = "css selector", value = "#app > div > div.text-center > div > div > div > ul > li.btns.clearfix > button.btn.btn-sm.btn-primary.btn-submit")$clickElement()
+# # Open Table: Health Center Service Delivery and Look-Alike Sites
+# remDr$findElement(using = "css selector", value = "#DataTables_Table_1 > tbody > tr:nth-child(1) > td.text-nowrap.dataTablesCenterAligned > a")$clickElement()
+# remDr$findElement(using = "css selector", value = "#DataTables_Table_1 > tbody > tr:nth-child(1) > td.text-nowrap.dataTablesCenterAligned")$clickElement()
+# 
+# # Navigate directly to query instead?
+# remDr$navigate("https://data.hrsa.gov/tools/data-explorer?paramServiceId=EHB%20Site&paramTyp=state&paramCd=17")
+# # Show / Hide Columns
+# remDr$findElement(using = "css selector", value = "#app > div > div.data-portal-area.table-view > div.table-responsive > div.find-data-head.clearfix > div.find-data-search.form-group.form-group-sm > div.tutorial-tooltip-container-2.dropdown > button")$clickElement()
+# 
+# # Export Data XLSX
+
+# Website/RSelenium keeps crashing while loading query
+
+# Alternatively, download nationwide dataset here:
+# https://data.hrsa.gov/DataDownload/DD_Files/Health_Center_Service_Delivery_and_LookAlike_Sites.csv
+
+download.file(
+  url = "https://data.hrsa.gov/DataDownload/DD_Files/Health_Center_Service_Delivery_and_LookAlike_Sites.csv",
+  destfile = "./data-raw/Health_Center_Service_Delivery_and_LookAlike_Sites.csv", # rename
+  mode = "wb"
+)
+
+# import Health_Center_Service_Delivery_and_LookAlike_Sites.csv and prep it for community_sites
 
 # rbind all sites into community_sites
 # reference
